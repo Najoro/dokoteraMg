59ca599299389516cdb276e7f69768e07a62aa37
Najoro Fanantenana <najofanantenana@gmail.com>
2025-02-25 08:11:30 +0100

    event fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,225 +5,175 @@ $(document).ready(function () {
       title: "Consultation",
       start: "2025-03-20",
       docteur: "Benja",
-      horaire: "10h 20 - 10h 15",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "10h 20 - 10h 15", 
     },
     {
       id: "2",
       title: "Médicament",
       start: "2025-03-20",
       docteur: "Alice",
-      horaire: "11h 00 - 11h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "11h 00 - 11h 45", 
     },
     {
       id: "3",
       title: "Radio",
       start: "2025-03-20",
       docteur: "Marc",
-      horaire: "14h 00 - 14h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "14h 00 - 14h 30", 
     },
     {
       id: "4",
       title: "Échographie",
       start: "2025-03-20",
       docteur: "Sophie",
-      horaire: "09h 00 - 09h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "09h 00 - 09h 30", 
     },
     {
       id: "5",
       title: "Prise de Sang",
       start: "2025-03-20",
       docteur: "Léa",
-      horaire: "15h 00 - 15h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "15h 00 - 15h 30", 
     },
     {
       id: "6",
       title: "Consultation de Suivi",
       start: "2025-01-25",
       docteur: "Tom",
-      horaire: "10h 00 - 10h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "10h 00 - 10h 30", 
     },
     {
       id: "7",
       title: "Vaccination",
       start: "2025-03-26",
       docteur: "Claire",
-      horaire: "13h 30 - 14h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "13h 30 - 14h 00", 
     },
     {
       id: "8",
       title: "Rééducation",
       start: "2025-06-27",
       docteur: "Paul",
-      horaire: "11h 15 - 11h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "11h 15 - 11h 45", 
     },
     {
       id: "9",
       title: "Consultation d'Urgence",
       start: "2025-12-28",
       docteur: "Nina",
-      horaire: "12h 00 - 12h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "12h 00 - 12h 30", 
     },
     {
       id: "10",
       title: "Prescription Médicale",
       start: "2025-03-01",
       docteur: "Max",
-      horaire: "14h 15 - 14h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "14h 15 - 14h 45", 
     },
     {
       id: "11",
       title: "Examen Clinique",
       start: "2025-03-12",
       docteur: "Eva",
-      horaire: "09h 30 - 10h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "09h 30 - 10h 00", 
     },
     {
       id: "12",
       title: "Analyse de Sang",
       start: "2025-03-19",
       docteur: "Alex",
-      horaire: "10h 30 - 11h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "10h 30 - 11h 00", 
     },
     {
       id: "13",
       title: "Consultation Psychologique",
       start: "2025-03-22",
       docteur: "Julia",
-      horaire: "13h 00 - 13h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "13h 00 - 13h 30", 
     },
     {
       id: "14",
       title: "Examen Radiologique",
       start: "2025-04-07",
       docteur: "Simon",
-      horaire: "15h 30 - 16h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "15h 30 - 16h 00", 
     },
     {
       id: "15",
       title: "Suivi Post-Opératoire",
       start: "2025-03-07",
       docteur: "Lina",
-      horaire: "11h 45 - 12h 15",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "11h 45 - 12h 15", 
     },
     {
       id: "16",
       title: "Évaluation Nutritionnelle",
       start: "2025-05-07",
       docteur: "Kévin",
-      horaire: "10h 15 - 10h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "10h 15 - 10h 45", 
     },
     {
       id: "17",
       title: "Consultation Gynécologique",
       start: "2025-09-08",
       docteur: "Chloé",
-      horaire: "12h 30 - 13h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "12h 30 - 13h 00", 
     },
     {
       id: "18",
       title: "Consultation Pédiatrique",
       start: "2025-03-10",
       docteur: "Julien",
-      horaire: "14h 45 - 15h 15",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "14h 45 - 15h 15", 
     },
     {
       id: "19",
       title: "Intervention Chirurgicale",
       start: "2025-02-10",
       docteur: "Laura",
-      horaire: "09h 15 - 09h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "09h 15 - 09h 45", 
     },
     {
       id: "20",
       title: "Évaluation Cardiaque",
       start: "2025-01-11",
       docteur: "Louis",
-      horaire: "10h 00 - 10h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "10h 00 - 10h 30", 
     },
     {
       id: "21",
       title: "Consultation Dermatologique",
       start: "2025-01-12",
       docteur: "Emma",
-      horaire: "11h 30 - 12h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "11h 30 - 12h 00", 
     },
     {
       id: "22",
       title: "Consultation Endocrinologique",
       start: "2026-01-13",
       docteur: "Mila",
-      horaire: "13h 15 - 13h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "13h 15 - 13h 45", 
     },
     {
       id: "23",
       title: "Séance de Kinésithérapie",
       start: "2026-03-14",
       docteur: "Lucas",
-      horaire: "14h 30 - 15h 00",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "14h 30 - 15h 00", 
     },
     {
       id: "24",
       title: "Consultation Cardiologique",
       start: "2025-02-15",
       docteur: "Sara",
-      horaire: "15h 15 - 15h 45",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "15h 15 - 15h 45", 
     },
     {
       id: "25",
       title: "Bilan de Santé",
       start: "2025-03-16",
       docteur: "Antoine",
-      horaire: "16h 00 - 16h 30",
-      startTime: '10:45:00',
-      endTime: '12:45:00'
+      horaire: "16h 00 - 16h 30", 
     },
   ];
 
